91473652630311862466c9bd434d2d003187d440
Marek Smid <marek@mareksmid.cz>
2019-06-19 10:58:02 +0200

    Some DB ORM fixes

--- lunchinator/commands.py
+++ lunchinator/commands.py
@@ -15,9 +15,7 @@ class Commands:
         self._sender = SlackSender()
 
     def select_meals(self, userid: str, meal_ids: list, recommended: bool):
-        user = User.objects.get(slack_id=userid)
-        if user is None:
-            user = User(slack_id=userid).save()
+        user = User.objects.get_or_create(slack_id=userid)
 
         for m_id in meal_ids:
             meal = Meal.objects.get(pk=m_id)
@@ -30,7 +28,7 @@ class Commands:
         self._sender.post_selections()
 
     def select_restaurants(self, userid: str, restaurant_ids: list):
-        user = self._user(userid)
+        user = User.objects.get_or_create(slack_id=userid)
 
         for r_id in restaurant_ids:
             restaurant = Restaurant.objects.get(pk=r_id)
@@ -39,27 +37,21 @@ class Commands:
         self._sender.send_meals(user)
 
     def erase(self, userid: str):
-        user = self._user(userid)
-        Selection.objects.filter(meal__date=date.today(), user=user).clear()
+        user = User.objects.get_or_create(slack_id=userid)
+        Selection.objects.filter(meal__date=date.today(), user=user).delete()
         self._sender.post_selections()
 
     def recommend(self, userid: str, number: int):
         self._sender.print_recommendation(self._get_recommendations(number, userid), userid)
 
     def list_restaurants(self, userid: str):
-        user = self._user(userid)
+        user = User.objects.get_or_create(slack_id=userid)
         self._sender.print_restaurants(userid, Restaurant.objects.all(), user.favorite_restaurants.all())
 
     def clear_restaurants(self, userid):
-        user = self._user(userid)
-        user.favorite_restaurants.clear()
+        user = User.objects.get_or_create(slack_id=userid)
+        user.favorite_restaurants.all().delete()
         self._sender.print_restaurants(userid, Restaurant.objects.all(), [])
 
     def _get_recommendations(self, number: int, userid: str) -> list:
         return []
-
-    def _user(self, userid: str) -> User:
-        user = User.objects.get(slack_id=userid)
-        if user is None:
-            user = User(slack_id=userid).save()
-        return user
