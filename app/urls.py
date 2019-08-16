94da6979790edbcc13f067643d6f6a6871957fea
Marek Smid <marek@mareksmid.cz>
2019-08-16 13:13:20 +0200

    Simplified static include

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from django.conf import settings
 from django.views.static import serve
 
@@ -7,5 +7,5 @@ urlpatterns = [
     path('jet/', include('jet.urls', 'jet')),
     path('admin/', admin.site.urls),
     path('lunchinator/', include('lunchinator.urls')),
-    re_path(r'^static/(?P<path>.*)/$', serve, {'document_root': settings.STATIC_ROOT})
+    path(f'{settings.STATIC_URL.strip("/")}/<path:path>', serve, {'document_root': settings.STATIC_ROOT})
 ]
