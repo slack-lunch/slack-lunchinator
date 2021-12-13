0aef3219e389e2c289ba44d6ce461667ccf90a65
kuzmoyev <ykuzmovych@luc.id>
2021-12-13 11:09:14 +0100

    Fix default for DEBUG

--- app/settings.py
+++ app/settings.py
@@ -3,7 +3,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-DEBUG = os.environ.get('DEBUG').lower() == 'true'
+DEBUG = os.environ.get('DEBUG', 'false').lower() == 'true'
 
 try:
     SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
